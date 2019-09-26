51a35a816acaea1c59b425e855d31a40d48529ca
Ahmed Khanzada <ahmed.khanzada@hyperlooptech.com>
2019-09-26 13:12:06 -0700

    Cleaning up mail file

--- enzuru/enzuru-mail.el
+++ enzuru/enzuru-mail.el
@@ -1,7 +1,9 @@
-;; wanderlust
+;; Autoloading
 (autoload 'wl "wl" "Wanderlust" t)
 (autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
 (autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
+(autoload 'wl-draft-send-with-imput-async "im-wl")
+(autoload 'wl-user-agent-compose "wl-draft" nil t)
 
 ;; IMAP
 (setq elmo-imap4-default-server "imap.gmail.com")
@@ -9,7 +11,6 @@
 (setq elmo-imap4-default-authenticate-type 'clear)
 (setq elmo-imap4-default-port '993)
 (setq elmo-imap4-default-stream-type 'ssl)
-
 (setq elmo-imap4-use-modified-utf7 t)
 
 ;; SMTP
@@ -18,20 +19,29 @@
 (setq wl-smtp-authenticate-type "plain")
 (setq wl-smtp-posting-user "me@enzu.ru")
 (setq wl-smtp-posting-server "smtp.gmail.com")
-(setq wl-local-domain "enzu.ru")
 
-(setq wl-default-folder "%inbox")
+;; Folders
 (setq wl-default-spec "%")
+(setq wl-default-folder "%inbox")
 (setq wl-draft-folder "%[Gmail]/Drafts") ; Gmail IMAP
 (setq wl-trash-folder "%[Gmail]/Trash")
 
+;; Asynchronous
+(setq wl-draft-send-function 'wl-draft-send-with-imput-async)
 (setq wl-folder-check-async t)
 
+;; Quicksearch
+(setq wl-quicksearch-folder "%[Gmail]/All Mail")
+
+;; Summary
+(setq wl-summary-indent-length-limit nil)
+(setq wl-summary-width nil)
+
+;; Details
+(setq wl-local-domain "enzu.ru")
 (setq wl-from "Ahmed Khanzada <me@enzu.ru>")
 
-(setq elmo-imap4-use-modified-utf7 t)
-
-(autoload 'wl-user-agent-compose "wl-draft" nil t)
+;; User agent
 (if (boundp 'mail-user-agent)
     (setq mail-user-agent 'wl-user-agent))
 (if (fboundp 'define-mail-user-agent)
@@ -42,10 +52,10 @@
       'wl-draft-kill
       'mail-send-hook))
 
-;; ignore  all fields
+;; Ignore  all fields...
 (setq wl-message-ignored-field-list '("^.*:"))
 
-;; ..but these five
+;; ...except these
 (setq wl-message-visible-field-list
 '("^To:"
   "^Cc:"
@@ -53,10 +63,5 @@
   "^Subject:"
   "^Date:"))
 
-(setq wl-summary-indent-length-limit nil)
-(setq wl-summary-width nil)
-
 ;; -comp.os.plan9:enzuru@eternal-september.org:119
 ;; (setq wl-draft-folder "+drafts")
-
-(setq wl-quicksearch-folder "%[Gmail]/All Mail")
